8c4aaacc0209745125e151d54c73ff506f3c65ed
Mike Wermuth <werm098@gmail.com>
2017-04-22 16:20:45 -0400

    Refactor: Cleanup Javascript code some

--- js/map.js
+++ js/map.js
@@ -31,9 +31,120 @@ function fillSelectWithOptions(select, options) {
   }
 }
 
+function openModal(contentDiv) {
+  document.getElementById('modal_content').innerHTML = contentDiv.innerHTML;
+  document.getElementById('modal').style.display = "block";
+}
+
+function openModalForm() {
+  openModal(document.getElementById('newPinFormContent'));
+}
+
+function closeModal() {
+  document.getElementById('modal').style.display = "none";
+}
+
 var map, newPinMarker, newMarkerWindow, markerInfoWindow;
 
-function setupCenterControl(map) {
+function initMap() {
+
+  var japan = {lat: 36.2048, lng: 138.2529};
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoomControl: true,
+    mapTypeControl: false,
+    scaleControl: false,
+    streetViewControl: false,
+    rotateControl: false,
+    fullscreenControl: true,
+    center: japan,
+    zoom: 5,
+    styles:
+     [{"elementType":"geometry","stylers":[{"color":"#212121"}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"elementType":"labels.text.fill","stylers":[{"color":"#757575"}]},{"elementType":"labels.text.stroke","stylers":[{"color":"#212121"}]},{"featureType":"administrative","elementType":"geometry","stylers":[{"color":"#757575"}]},{"featureType":"administrative.country","elementType":"labels.text.fill","stylers":[{"color":"#9e9e9e"}]},{"featureType":"administrative.land_parcel","stylers":[{"visibility":"off"}]},{"featureType":"administrative.locality","elementType":"labels.text.fill","stylers":[{"color":"#bdbdbd"}]},{"featureType":"administrative.neighborhood","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels.text.fill","stylers":[{"color":"#757575"}]},{"featureType":"poi.business","stylers":[{"visibility":"off"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#181818"}]},{"featureType":"poi.park","elementType":"labels.text.fill","stylers":[{"color":"#616161"}]},{"featureType":"poi.park","elementType":"labels.text.stroke","stylers":[{"color":"#1b1b1b"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"color":"#c50000"}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#c50000"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#8a8a8a"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#373737"}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#c50000"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#c50000"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#3c3c3c"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#c50000"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#c50000"}]},{"featureType":"road.highway.controlled_access","elementType":"geometry","stylers":[{"color":"#4e4e4e"}]},{"featureType":"road.highway.controlled_access","elementType":"geometry.fill","stylers":[{"color":"#c50000"}]},{"featureType":"road.highway.controlled_access","elementType":"geometry.stroke","stylers":[{"color":"#c50000"}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"#c50000"}]},{"featureType":"road.local","elementType":"geometry.stroke","stylers":[{"color":"#c50000"}]},{"featureType":"road.local","elementType":"labels.text.fill","stylers":[{"color":"#616161"}]},{"featureType":"transit","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"labels.text.fill","stylers":[{"color":"#757575"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"}]},{"featureType":"water","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"color":"#3d3d3d"}]}]
+  });
+
+  setupBottomCenterControl(map);
+  var logoDiv = document.createElement('div');
+  logoDiv.style['color'] = 'white';
+  logoDiv.innerHTML = "<h1>Kitsune Network!</h1>";
+  map.controls[google.maps.ControlPosition.TOP].push(logoDiv);
+
+  newMarkerWindow = new google.maps.InfoWindow({
+    content: document.getElementById('newPinMessage')
+  })
+  google.maps.event.addListener(newMarkerWindow, 'closeclick', function() {
+    newPinMarker.setMap(null);
+  });
+
+  // get and setup markers
+  ajax({
+    method: 'GET',
+    relativeURL: '/pins',
+    success: setupMarkers,
+    error: function() {
+      // There was a connection error of some sort
+    }
+  });
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      map.setCenter(pos);
+    }, function() {
+      // User probbaly did not give permission for their location
+    });
+  } else {
+    // Browser doesn't support Geolocation
+  }
+
+}
+
+function setupMarkers(json) {
+  var markers = json.map(function(pin, i) {
+    var location = {lat: pin.lat, lng: pin.lng};
+    var marker =  new google.maps.Marker({
+      position: location,
+    });
+    marker.addListener('click', function() {
+      showPinInfoWindow(pin, marker);
+    });
+    return marker;
+  });
+  var markerCluster = new MarkerClusterer(map, markers,
+      {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+}
+
+function showPinInfoWindow(pin, marker) {
+  ajax({
+    method: 'GET',
+    relativeURL: '/pin/' + pin.id,
+    success: function(json) {
+      var content = document.createElement('div');
+      for (var jsonKey in json) {
+        // continue if the property is from prototype
+        if (!json.hasOwnProperty(jsonKey)) continue;
+        var p = document.createElement('p');
+        p.innerHTML = jsonKey + ": " + json[jsonKey];
+        content.appendChild(p);
+      }
+      if (markerInfoWindow !== undefined) {
+        markerInfoWindow.close()
+      }
+      markerInfoWindow = new google.maps.InfoWindow({
+        content: content
+      })
+      markerInfoWindow.open(map, marker);
+    },
+    error: function() {
+      // There was a connection error of some sort
+    }
+  });
+}
+
+function setupBottomCenterControl(map) {
 
   var controlDiv = document.createElement('div');
   controlDiv.style['padding-bottom'] = '10px';
@@ -75,110 +186,6 @@ function setupCenterControl(map) {
 
   map.controls[google.maps.ControlPosition.BOTTOM_CENTER].push(controlDiv);
 
-}
-
-function initMap() {
-
-  var japan = {lat: 36.2048, lng: 138.2529};
-  map = new google.maps.Map(document.getElementById('map'), {
-    zoomControl: true,
-    mapTypeControl: false,
-    scaleControl: false,
-    streetViewControl: false,
-    rotateControl: false,
-    fullscreenControl: true,
-    center: japan,
-    zoom: 5,
-    styles:
-     [{"elementType":"geometry","stylers":[{"color":"#212121"}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"elementType":"labels.text.fill","stylers":[{"color":"#757575"}]},{"elementType":"labels.text.stroke","stylers":[{"color":"#212121"}]},{"featureType":"administrative","elementType":"geometry","stylers":[{"color":"#757575"}]},{"featureType":"administrative.country","elementType":"labels.text.fill","stylers":[{"color":"#9e9e9e"}]},{"featureType":"administrative.land_parcel","stylers":[{"visibility":"off"}]},{"featureType":"administrative.locality","elementType":"labels.text.fill","stylers":[{"color":"#bdbdbd"}]},{"featureType":"administrative.neighborhood","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels.text.fill","stylers":[{"color":"#757575"}]},{"featureType":"poi.business","stylers":[{"visibility":"off"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#181818"}]},{"featureType":"poi.park","elementType":"labels.text.fill","stylers":[{"color":"#616161"}]},{"featureType":"poi.park","elementType":"labels.text.stroke","stylers":[{"color":"#1b1b1b"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"color":"#c50000"}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#c50000"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#8a8a8a"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#373737"}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#c50000"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#c50000"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#3c3c3c"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#c50000"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#c50000"}]},{"featureType":"road.highway.controlled_access","elementType":"geometry","stylers":[{"color":"#4e4e4e"}]},{"featureType":"road.highway.controlled_access","elementType":"geometry.fill","stylers":[{"color":"#c50000"}]},{"featureType":"road.highway.controlled_access","elementType":"geometry.stroke","stylers":[{"color":"#c50000"}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"#c50000"}]},{"featureType":"road.local","elementType":"geometry.stroke","stylers":[{"color":"#c50000"}]},{"featureType":"road.local","elementType":"labels.text.fill","stylers":[{"color":"#616161"}]},{"featureType":"transit","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"labels.text.fill","stylers":[{"color":"#757575"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"}]},{"featureType":"water","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"color":"#3d3d3d"}]}]
-  });
-
-  setupCenterControl(map);
-  var logoDiv = document.createElement('div');
-  logoDiv.style['color'] = 'white';
-  logoDiv.innerHTML = "<h1>Kitsune Network!</h1>";
-  map.controls[google.maps.ControlPosition.TOP].push(logoDiv);
-
-  newMarkerWindow = new google.maps.InfoWindow({
-    content: document.getElementById('newPinMessage')
-  })
-  google.maps.event.addListener(newMarkerWindow, 'closeclick', function() {
-    newPinMarker.setMap(null);
-  });
-
-  // Try HTML5 geolocation.
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-      map.setCenter(pos);
-    }, function() {
-      // User probbaly did not give permission for their location
-    });
-  } else {
-    // Browser doesn't support Geolocation
-  }
-
-  ajax({
-    method: 'GET',
-    relativeURL: '/pins',
-    success: function(json) {
-      var markers = json.map(function(pin, i) {
-        var location = {lat: pin.lat, lng: pin.lng};
-        var marker =  new google.maps.Marker({
-          position: location,
-        });
-        marker.addListener('click', function() {
-          ajax({
-            method: 'GET',
-            relativeURL: '/pin/' + pin.id,
-            success: function(json) {
-              var content = document.createElement('div');
-              for (var jsonKey in json) {
-                // continue if the property is from prototype
-                if (!json.hasOwnProperty(jsonKey)) continue;
-                var p = document.createElement('p');
-                p.innerHTML = jsonKey + ": " + json[jsonKey];
-                content.appendChild(p);
-              }
-              if (markerInfoWindow !== undefined) {
-                markerInfoWindow.close()
-              }
-              markerInfoWindow = new google.maps.InfoWindow({
-                content: content
-              })
-              markerInfoWindow.open(map, marker);
-            },
-            error: function() {
-              // There was a connection error of some sort
-            }
-          });
-        });
-        return marker;
-      });
-      var markerCluster = new MarkerClusterer(map, markers,
-          {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
-    },
-    error: function() {
-      // There was a connection error of some sort
-    }
-  });
-
-}
-
-function openModal(contentDiv) {
-  document.getElementById('modal_content').innerHTML = contentDiv.innerHTML;
-  document.getElementById('modal').style.display = "block";
-}
-
-function openModalForm() {
-  openModal(document.getElementById('newPinFormContent'));
-}
-
-function closeModal() {
-  document.getElementById('modal').style.display = "none";
 }
 
 function sendActivationEmail() {
