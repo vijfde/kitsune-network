b56d3704655046537a536bb53c45990c5f7edd1f
Mike Wermuth <werm098@gmail.com>
2017-05-04 18:29:10 -0400

    Refactor: Moved ManagePinHandler back into main.py

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ import base64
 import uuid
 
 from kitsunemap_entities import Pin
-from ManagePinHandler import ManagePinHandler
 import const_data
 import credentials
 
@@ -104,6 +103,42 @@ class PinEditRequestHandler(webapp2.RequestHandler):
             edit_pin.put()
             send_email(edit_pin.email, edit_pin.access_uuid, True)
         template_values = { 'action': 'edit' }
+        template = JINJA_ENVIRONMENT.get_template('templates/email_sent.html')
+        self.response.write(template.render(template_values))
+
+class ManagePinHandler(webapp2.RequestHandler):
+    def put(self):
+        edit_pin_uuid = self.request.POST.get('editAccessUUID')
+        pin = Pin.query(Pin.access_uuid == edit_pin_uuid).get()
+        if pin == None:
+            self.response.set_status(404)
+            self.response.out.write("")
+            return
+
+        form_error_message = Pin.validate_pin_values(self.request.POST, False)
+        if form_error_message:
+            self.response.set_status(400)
+            self.response.out.write(form_error_message)
+            return
+
+        pin.set_pin_values(self.request.POST, self.request.remote_addr, False)
+
+        template = JINJA_ENVIRONMENT.get_template('templates/pin_updated.html')
+        self.response.write(template.render())
+
+    def post(self):
+        form_error_message = Pin.validate_pin_values(self.request.POST, True)
+        if form_error_message:
+            self.response.set_status(400)
+            self.response.out.write(form_error_message)
+            return
+
+        new_pin = Pin()
+        new_pin.set_pin_values(self.request.POST, self.request.remote_addr, True)
+
+        send_email(new_pin.email, new_pin.access_uuid, False)
+
+        template_values = { 'action': 'activate' }
         template = JINJA_ENVIRONMENT.get_template('templates/email_sent.html')
         self.response.write(template.render(template_values))
 
