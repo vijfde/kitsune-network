81b6ed5d7f0104dd591e763a5ab18fdd8aa4284b
Mike Wermuth <werm098@gmail.com>
2017-05-08 22:14:56 -0400

    Added: Japanese names for members and songs

--- constants.py
+++ constants.py
@@ -1,45 +1,42 @@
 # -*- coding: utf-8 -*-
 members = {
-    '1': 'SU-METAL',
-    '2': 'MOAMETAL',
-    '3': 'YUIMETAL',
-    '0': 'They are all my favorite',
+    '1': 'スーメタル / SU-METAL / Queen Su / Goddess'.decode('utf8'),
+    '2': 'モアメタル / MOAMETAL / Angel of Love'.decode('utf8'),
+    '3': 'ユイメタル / YUIMETAL / Angel of Dance'.decode('utf8'),
 }
-members_display_sort = ['0', '1', '2', '3']
+members_display_sort = ['1', '2', '3']
 songs = {
-    "0": "They are all my favorite",
-    "1": "Doki Doki ☆ Morning".decode('utf8'),
-    "2": "Ijime Dame Zettai",
-    "3": "Akatsuki",
-    "4": "Iine!",
-    "5": "Head Bangya!!",
-    "6": "Uki Uki ★ Midnight".decode('utf8'),
+    "1": "Doki Doki ☆ Morning / ド・キ・ド・キ☆モーニング".decode('utf8'),
+    "2": "Ijime Dame Zettai / イジメ、ダメ、ゼッタイ".decode('utf8'),
+    "3": "Akatsuki / 紅月-アカツキ-".decode('utf8'),
+    "4": "Iine! / いいね".decode('utf8'),
+    "5": "Head Bangya!! / ヘドバンギャー! !".decode('utf8'),
+    "6": "Uki Uki ★ Midnight / ウ・キ・ウ・キ★ミッドナイト".decode('utf8'),
     "7": "BABYMETAL DEATH",
-    "8": "Onedari Daisakusen",
+    "8": "Onedari Daisakusen / おねだり大作戦".decode('utf8'),
     "9": "Catch me if you can",
-    "10": "Megitsune",
+    "10": "Megitsune / メギツネ".decode('utf8'),
     "11": "NO RAIN, NO RAINBOW",
-    "12": "Gimme Chocolate!!",
-    "13": "Rondo of Nightmare",
-    "14": "Song 4",
+    "12": "Gimme Chocolate!! / ギミチョコ！！".decode('utf8'),
+    "13": "Rondo of Nightmare / 悪夢の輪舞曲".decode('utf8'),
+    "14": "Song 4 / 4の歌".decode('utf8'),
     "15": "Road of Resistance",
-    "16": "Awadama Fever",
-    "17": "YAVA!",
+    "16": "Awadama Fever / あわだまフィーバー".decode('utf8'),
+    "17": "YAVA! / ヤバッ！".decode('utf8'),
     "18": "THE ONE",
     "19": "Karate",
-    "20": "Amore",
-    "21": "Meta Tarō".decode('utf8'),
-    "22": "Syncopation",
+    "20": "Amore / Amore - 蒼星 -".decode('utf8'),
+    "21": "Meta Tarō / META！メタ太郎".decode('utf8'),
+    "22": "Syncopation / シンコペーション".decode('utf8'),
     "23": "From Dusk Till Dawn",
     "24": "GJ!",
     "25": "Sis. Anger",
     "26": "Tales of The Destinies"
 }
-songs_display_sort = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26']
+songs_display_sort = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26']
 communities = {
-    "0": "None",
     "1": "THE ONE",
     "2": "/r/BABYMETAL",
     "3": "/r/BABYMETAL_japan"
 }
-communities_display_sort = ['0', '1', '2', '3']
+communities_display_sort = ['1', '2', '3']
