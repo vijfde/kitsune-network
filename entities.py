17c534ab3dae413237cc0cb64dc8552f59f590f0
Mike Wermuth <werm098@gmail.com>
2017-05-11 16:55:28 -0400

    Fixed: Don’t show “none” if other communities were also selected in the Discord notification

--- entities.py
+++ entities.py
@@ -26,7 +26,10 @@ class Pin(ndb.Model):
         pin_details = 'Title: ' + self.name
         pin_details += '\nFav Song: ' + constants.songs[str(self.favorite_song)]
         pin_details += '\nFav Member: ' + constants.members[str(self.favorite_member)]
-        pin_details += '\nCommunities: ' + ', '.join([constants.communities[str(community)] for community in self.communities.split(',')])
+        communities = self.communities.split(',')
+        if len(communities) > 1 and '0' in communities:
+            communities.remove('0')
+        pin_details += '\nCommunities: ' + ', '.join([constants.communities[str(community)] for community in communities])
         pin_details += '\nAbout: \n' + self.about_you
         content = """**New Pin Activated!**```%s```""" % pin_details
         task = taskqueue.add(
